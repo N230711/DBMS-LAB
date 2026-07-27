5e3fcf7ed0cb9b373c8dec87772836e99da41c89
N230711 <n230711@rguktn.ac.in>
2026-07-27 22:14:01 +0530

    WEEK 2 LAB SUBMISSION

--- N230711/WEEK-2-LAB/week2_functions.sql
+++ N230711/WEEK-2-LAB/week2_functions.sql
@@ -7,6 +7,8 @@ select *from certificate_application;
 select *from panchayat_office;
 select upper(full_name) as UPPER_CASE from citizen ;
 select lower(village_name) as lower_case from citizen;
+UPDATE `gram_panchayath_db`.`certificate_type` SET `certificate_name` = 'No-Dues Certificate' WHERE (`certificate_type_id` = '6');
+UPDATE `gram_panchayath_db`.`certificate_application` SET `certificate_name` = 'Birth Record Request' WHERE (`application_id` = '1006');
 select length(full_name) as Length from citizen;
 select substring(reference_number,1,4) as reference_number from certificate_application;
 select concat(full_name,' - ',village_name) as full_name__village_name from citizen;
@@ -29,3 +31,46 @@ select current_date(),current_time()as date_time;
 select year(application_date) as application_year from certificate_application;
 select month(application_date) as application_month from certificate_application;
 select day(application_date) as application_day from certificate_application;
+/*SELECT 
+    ca.appliction_id,
+    ca.citizen_id,
+    ca.certificate_name,
+    ca.application_date,
+    ct.processing_days,
+    DATE_ADD(ca.application_date, INTERVAL ct.processing_days DAY) AS expected_issue_date
+FROM 
+    certificate_application ca
+JOIN 
+    certificate_type ct 
+ON 
+    ca.certificate_name = ct.certificate_name;*/
+SELECT application_id, application_date, processing_days, DATE_ADD(application_date, INTERVAL processing_days DAY) AS expected_issue_date FROM certificate_application a,certificate_type c where a.certificate_name = c.certificate_name;
+/*SELECT 
+   a.application_id,
+    a.certificate_name,
+    a.application_date,
+    (
+        SELECT c.processing_days 
+        FROM certificate_type c 
+        WHERE c.certificate_name = a.certificate_name
+    ) AS processing_days,
+    DATE_ADD(
+        a.application_date, 
+        INTERVAL (
+            SELECT c.processing_days 
+            FROM certificate_type c 
+            WHERE c.certificate_name = a.certificate_name
+        ) DAY
+    ) AS expected_issue_date
+FROM certificate_application a;*/
+select application_id , application_date , date_add(application_date ,interval 30 day) as adding_30days from certificate_application;
+select application_id , application_date , date_add(application_date ,interval -7 day) as subtracting_7days from certificate_application;
+select application_id, application_date, datediff(current_date(),application_date) as no_of_days from certificate_application;
+UPDATE `gram_panchayath_db`.`certificate_application` SET `application_date` = '2026-07-04' WHERE (`application_id` = '1004');
+select *from certificate_application where year(application_date)= year(current_date());
+select convert(application_fees , signed ) as converted_int from certificate_type;
+select cast(processing_days as char) as converted_char from certificate_type;
+select convert(application_date,datetime) as converted_datetime from certificate_application;
+select cast(processing_days as decimal) as converted_decimal from certificate_type;
+select cast(application_fees as char)as converted_char from certificate_type;
+select cast('12.3' as decimal)+23.6 as addition;
